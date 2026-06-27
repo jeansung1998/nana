f316ceec4abfdd2d5a79be49f48fed30cecb07ef
ryker <jeansung1998@gmail.com>
2026-06-27 21:05:25 +0900

    fix: Stream 재추가

--- main.py
+++ main.py
@@ -142,11 +142,16 @@ def twiml():
     data = call_scripts.get(call_id, {})
     intro = data.get("intro", "안녕하세요. 나나입니다.")
 
+    ws_url = BASE_URL.replace('https://', 'wss://').replace('http://', 'ws://')
+
     xml = f"""<?xml version="1.0" encoding="UTF-8"?>
 <Response>
   <Say language="ko-KR">{intro}</Say>
-  <Hangup/>
+  <Connect>
+    <Stream url="{ws_url}/stream?id={call_id}"/>
+  </Connect>
 </Response>"""
+    
     return Response(xml, mimetype="text/xml")
 
 @sock.route("/stream")
